fbb97119d40755e2439755f7217784c498293a95
Piton <andre.elias.vieira@gmail.com>
2020-10-10 15:28:11 -0300

    Added navigator_page.dart and home_widgets.dart, also modified README.md

---
+++ GetClub/lib/widgets/home_widgets.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class Home_Widgets extends StatefulWidget {
+  @override
+  _Home_WidgetsState createState() => _Home_WidgetsState();
+}
+
+// ignore: camel_case_types
+class _Home_WidgetsState extends State<Home_Widgets> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
