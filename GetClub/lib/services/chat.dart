a788e6066c2da7102b26c15d98785fb4408bca32
Piton <andre.elias.vieira@gmail.com>
2020-10-12 18:47:41 -0300

    Added readme

--- GetClub/lib/services/chat.dart
+++ GetClub/lib/services/chat.dart
@@ -1,53 +1,151 @@
-// host (dar o io ) localhost:3333
-// on (como recebera da API)
-// emit (enviar para API)
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
 
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/status.dart' as status;
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
 
-class UserCreate extends StatefulWidget {
-  sendMessage(String msg) async {
-    var chatConnection = IOWebSocketChannel.connect("http://localhost:3333");
+class _ChatPageState extends State<ChatPage> {
+  SocketIO socketIO;
+  List<String> messages;
+  double height, width;
+  TextEditingController textController;
+  ScrollController scrollController;
 
-    chatConnection.stream.listen((message) {
-      chatConnection.sink.add("{"
-          '"sender":"5f846feffb1b6f2e18cb9ff2",'
-          '"addressee": "5f846718feede3342fcd3f66",'
-          '"sendMessage":"$msg"'
-          "}");
-      chatConnection.sink.close(status.goingAway);
+  @override
+  void initState() {
+    //Initializing the message list
+    messages = List<String>();
+    //Initializing the TextEditingController and ScrollController
+    textController = TextEditingController();
+    scrollController = ScrollController();
+    //Creating the socket
+    socketIO = SocketIOManager().createSocketIO(
+      'http://localhost:3333',
+      '/',
+    );
+    //Call init before doing anything with socket
+    socketIO.init();
+    //Subscribe to an event to listen to
+    socketIO.subscribe('receive_message', (jsonData) {
+      //Convert the JSON data received into a Map
+      Map<String, dynamic> data = json.decode(jsonData);
+      this.setState(() => messages.add(data['message']));
+      scrollController.animateTo(
+        scrollController.position.maxScrollExtent,
+        duration: Duration(milliseconds: 600),
+        curve: Curves.ease,
+      );
     });
+    //Connect to the socket
+    socketIO.connect();
+    super.initState();
   }
 
-  // logar ja cria usuario
-  @override
-  _UserCreateState createState() => _UserCreateState();
-}
-
-class _UserCreateState extends State<UserCreate> {
-  @override
-  Widget build(BuildContext context) {
-    return new StreamBuilder(builder: (context, snapshot) {
-      //return 0;
-    });
+  Widget buildSingleMessage(int index) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        margin: const EdgeInsets.only(bottom: 20.0, left: 20.0),
+        decoration: BoxDecoration(
+          color: Colors.deepPurple,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Text(
+          messages[index],
+          style: TextStyle(color: Colors.white, fontSize: 15.0),
+        ),
+      ),
+    );
   }
-}
 
-// quando tiver logado, e estiver acessando a rota protegida, mandar token
-// em service
+  Widget buildMessageList() {
+    return Container(
+      height: height * 0.8,
+      width: width,
+      child: ListView.builder(
+        controller: scrollController,
+        itemCount: messages.length,
+        itemBuilder: (BuildContext context, int index) {
+          return buildSingleMessage(index);
+        },
+      ),
+    );
+  }
 
-class ChatService extends StatefulWidget {
-  @override
-  _ChatServiceState createState() => _ChatServiceState();
-}
+  Widget buildChatInput() {
+    return Container(
+      width: width * 0.7,
+      padding: const EdgeInsets.all(2.0),
+      margin: const EdgeInsets.only(left: 40.0),
+      child: TextField(
+        decoration: InputDecoration.collapsed(
+          hintText: 'Send a message...',
+        ),
+        controller: textController,
+      ),
+    );
+  }
 
-class _ChatServiceState extends State<ChatService> {
+  Widget buildSendButton() {
+    return FloatingActionButton(
+      backgroundColor: Colors.deepPurple,
+      onPressed: () {
+        //Check if the textfield has text or not
+        if (textController.text.isNotEmpty) {
+          //Send the message as JSON data to send_message event
+          socketIO.sendMessage(
+              'send_message', json.encode({'message': textController.text}));
+          //Add the message to the list
+          this.setState(() => messages.add(textController.text));
+          textController.text = '';
+          //Scrolldown the list to show the latest message
+          scrollController.animateTo(
+            scrollController.position.maxScrollExtent,
+            duration: Duration(milliseconds: 600),
+            curve: Curves.ease,
+          );
+        }
+      },
+      child: Icon(
+        Icons.send,
+        size: 30,
+      ),
+    );
+  }
+
+  Widget buildInputArea() {
+    return Container(
+      height: height * 0.1,
+      width: width,
+      child: Row(
+        children: <Widget>[
+          buildChatInput(),
+          buildSendButton(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: height * 0.1),
+            buildMessageList(),
+            buildInputArea(),
+          ],
+        ),
+      ),
+    );
   }
 }
